19364d348ba09fe9274b9486e982947afca90cf8
iddqd-dev <60209821+iddqd-dev@users.noreply.github.com>
2023-03-23 23:46:47 +0300

    Добавлена проверка для исключения возможности дублирования QR в папке пользователя.

--- core/config.py
+++ core/config.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 from core.logger import create_logger
 
 
-TOKEN = ''
+TOKEN = '6001789387:AAGbfcyEgEYIhEvzLjbPNeBN-Bb6g0pXiVk'
 logger = create_logger()
 bot = Bot(TOKEN)
 dispatcher = Dispatcher(bot)
